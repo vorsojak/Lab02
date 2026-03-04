7332b8532a0345124eef15b18328bbbaae2333bd
vorsojak <valerio.orsogiacone@gmail.com>
2026-03-04 10:48:06 +0100

    svolgimento

--- main.py
+++ main.py
@@ -7,19 +7,52 @@ while(True):
 
     t.printMenu()
 
-    t.loadDictionary("filename.txt")
+    t.loadDictionary("dictionary.txt")
 
-    txtIn = input()
 
-    # Add input control here!
+    txtIn = input("Seleziona l'opzione: ")
+    try:
+         int(txtIn)
+    except (TypeError, ValueError) as e:
+        print(f"Input non valido, scegli un opazione da 1 a 5!")
+        continue
 
     if int(txtIn) == 1:
-        print()
-        txtIn = input()
-        pass
+        while True:
+            print()
+            txtIn = input("Inserisci la nuova parola nel formato 'parolaAliena Traduzione'."
+                          " Se vuoi tornare al menu clicca +.\n").strip()
+            if txtIn == "+":
+                break
+            txtIn = txtIn.split()
+            if len(txtIn) != 2:
+                print("Errore: formato errato!")
+                continue
+            else:
+                t.handleAdd(txtIn)
+        continue
     if int(txtIn) == 2:
-        pass
+        while True:
+            print()
+            txtIn = input("Inserisci la parola aliena da cercare."
+                          " Se vuoi tornare al menu clicca +.\n").strip()
+            if txtIn == "+":
+                break
+            t.handleTranslate(txtIn)
+        continue
+
     if int(txtIn) == 3:
-        pass
+        while True:
+            print()
+            txtIn = input("Inserisci la parola aliena con WildCard da cercare (es. par?la)."
+                          " Se vuoi tornare al menu clicca +.\n").strip()
+            if txtIn == "+":
+                break
+            t.handleWildCard(txtIn)
+        continue
     if int(txtIn) == 4:
+        t.stampDictionary()
+        pass
+    if int(txtIn) == 5:
+        print("Ciao!")
         break
--- translator.py
+++ translator.py
@@ -1,27 +1,64 @@
+import fnmatch
+from dataclasses import dataclass
+
+
+@dataclass
 class Translator:
 
     def __init__(self):
-        pass
+        self.dizionario = {}
+        dizionario: dict
 
     def printMenu(self):
         # 1. Aggiungi nuova parola
+        print("1. Aggiungi una nuova parola")
         # 2. Cerca una traduzione
+        print("2. Cerca una traduzione")
         # 3. Cerca con wildcard
-        # 4. Exit
+        print("3. Cerca con wildcard")
+        # 4. Stampa tutto il dizionario
+        print("4. Stampa tutto il dizionario")
+        # 5. Exit
+        print("5. Exit")
         pass
 
     def loadDictionary(self, dict):
         # dict is a string with the filename of the dictionary
-        pass
+        with open(dict, "r", encoding="utf-8") as f:
+            for r in f:
+                riga = r.strip().split()
+                #print(riga)
+                parolaAliena = riga[0]
+                parolaItaliana = riga[1]
+                self.dizionario[parolaAliena] = parolaItaliana
+            #print(self.dizionario)
+        return self.dizionario
 
     def handleAdd(self, entry):
-        # entry is a tuple <parola_aliena> <traduzione1 traduzione2 ...>
-        pass
+        self.dizionario[entry[0]] = entry[1]
+        with open("dictionary.txt", "a", encoding="utf-8") as f:
+            f.write(f"\n{entry[0]} {entry[1]}")
+
+        print(f"{entry[0]} correttamente aggiunta al dizionario!")
+        return
+
 
     def handleTranslate(self, query):
-        # query is a string <parola_aliena>
-        pass
+        traduzione = self.dizionario.get(query)
+        if traduzione is None:
+            print("Parola non presente nel dizionario.")
+            return
+        print(f"La traduzione di {query} è {traduzione}.")
+        return
 
     def handleWildCard(self,query):
-        # query is a string with a ? --> <par?la_aliena>
-        pass
+        for k, v in self.dizionario.items():
+            if fnmatch.fnmatch(k, query):
+                print(f"Parola trovata: {k}. La traduzione è {v}.")
+                return
+        print("Nessuna parola trovata")
+        return
+
+    def stampDictionary(self):
+        for k,v in self.dizionario.items():
+            print(f"{k} - {v}")
